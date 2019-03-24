abd6893cdc86ebb31f4550dbc94a7e43dc59f0b3
Ian Shipman <ics@gambolingpangolin.com>
2019-03-24 10:37:38 -0500

    adds test cases

--- test/Main.hs
+++ test/Main.hs
@@ -22,15 +22,27 @@ preludeTests :: TestTree
 preludeTests = testGroup "prelude"
     [ testCase "I should compute the identity" $
         runMachine0 (Ap (Ident "I") (Nmbr 1)) @=? Right (Just 1)
+
     , testCase "K should project the first arg" $
         runMachine0 (Ap (Ap (Ident "K") (Nmbr 1)) (Nmbr 2)) @=? Right (Just 1)
-    , testCase "K2 should project the second arg" $
+
+    , testCase "K1 should project the second arg" $
         runMachine0 (Ap (Ap (Ident "K1") (Nmbr 1)) (Nmbr 2)) @=? Right (Just 2)
+
     ]
 
 
 boolTests :: TestTree
 boolTests = testGroup "bool"
-    [ testCase "true" $ runMachineBool (B.toNumber B.true) @=? Right (Just 1)
-    , testCase "false" $ runMachineBool (B.toNumber B.false) @=? Right (Just 0)
+    [ testCase "true" $ runMachineBool (B.toNumber B.true) @=? trueVal
+    , testCase "false" $ runMachineBool (B.toNumber B.false) @=? falseVal
+    , testCase "not false" $ runMachineBool (B.toNumber $ B.apNot B.false) @=? trueVal
+    , testCase "true and false" $ runMachineBool (B.toNumber $ B.true `B.apAnd` B.false) @=? falseVal
+    , testCase "true and true" $ runMachineBool (B.toNumber $ B.true `B.apAnd` B.true) @=? trueVal
+    , testCase "true or false" $ runMachineBool (B.toNumber $ B.true `B.apOr` B.false) @=? trueVal
+    , testCase "false or false" $ runMachineBool (B.toNumber $ B.false `B.apOr` B.false) @=? falseVal
     ]
+
+    where
+    trueVal = Right (Just 1)
+    falseVal = Right (Just 0)

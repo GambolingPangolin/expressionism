f6b6546dc78714d8b9f674275ce6caabc77e5f60
Ian Shipman <ics@gambolingpangolin.com>
2019-03-24 13:19:02 -0500

    more name changes

--- test/Main.hs
+++ test/Main.hs
@@ -5,7 +5,7 @@ import           Test.Tasty.HUnit           (testCase, (@=?))
 
 import           Expressionism              (Expr (..), preludeDefs)
 import qualified Expressionism.Bool         as B
-import           Expressionism.GraphReducer (execBounded, initMachine, result)
+import           Expressionism.GraphReducer (execBounded, initMachine, result, MachineError)
 
 
 main :: IO ()
@@ -18,31 +18,35 @@ runMachine0 = flip runMachine preludeDefs
 runMachineBool = flip runMachine B.preludeBool
 
 
+retVal :: Int -> Either MachineError (Maybe Int)
+retVal i = Right $ Just i
+
+
 preludeTests :: TestTree
 preludeTests = testGroup "prelude"
     [ testCase "I should compute the identity" $
-        runMachine0 (Ap (Ident "I") (Nmbr 1)) @=? Right (Just 1)
+        runMachine0 (Ap (Ident "I") (Nmbr 1)) @=? retVal 1
 
     , testCase "K should project the first arg" $
-        runMachine0 (Ap (Ap (Ident "K") (Nmbr 1)) (Nmbr 2)) @=? Right (Just 1)
+        runMachine0 (Ap (Ap (Ident "K") (Nmbr 1)) (Nmbr 2)) @=? retVal 1
 
     , testCase "K1 should project the second arg" $
-        runMachine0 (Ap (Ap (Ident "K1") (Nmbr 1)) (Nmbr 2)) @=? Right (Just 2)
+        runMachine0 (Ap (Ap (Ident "K1") (Nmbr 1)) (Nmbr 2)) @=? retVal 2
 
     ]
 
 
 boolTests :: TestTree
 boolTests = testGroup "bool"
-    [ testCase "true" $ runMachineBool (B.toNumber B.true) @=? trueVal
-    , testCase "false" $ runMachineBool (B.toNumber B.false) @=? falseVal
-    , testCase "not false" $ runMachineBool (B.toNumber $ B.apNot B.false) @=? trueVal
-    , testCase "true and false" $ runMachineBool (B.toNumber $ B.true `B.apAnd` B.false) @=? falseVal
-    , testCase "true and true" $ runMachineBool (B.toNumber $ B.true `B.apAnd` B.true) @=? trueVal
-    , testCase "true or false" $ runMachineBool (B.toNumber $ B.true `B.apOr` B.false) @=? trueVal
-    , testCase "false or false" $ runMachineBool (B.toNumber $ B.false `B.apOr` B.false) @=? falseVal
+    [ testCase "true" $ runMachineBool (B.toNumber B.true) @=? retTrue
+    , testCase "false" $ runMachineBool (B.toNumber B.false) @=? retFalse
+    , testCase "not false" $ runMachineBool (B.toNumber $ B.apNot B.false) @=? retTrue
+    , testCase "true and false" $ runMachineBool (B.toNumber $ B.true `B.apAnd` B.false) @=? retFalse
+    , testCase "true and true" $ runMachineBool (B.toNumber $ B.true `B.apAnd` B.true) @=? retTrue
+    , testCase "true or false" $ runMachineBool (B.toNumber $ B.true `B.apOr` B.false) @=? retTrue
+    , testCase "false or false" $ runMachineBool (B.toNumber $ B.false `B.apOr` B.false) @=? retFalse
     ]
 
     where
-    trueVal = Right (Just 1)
-    falseVal = Right (Just 0)
+    retTrue = retVal 1
+    retFalse = retVal 0
